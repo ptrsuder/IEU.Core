068a7fb094e4f6e6eef7879d11def4a857172f45
ptrsuder <ptrsuder@gmail.com>
2020-07-21 19:13:04 +0300

    Add new arch convertation

--- IEU.Core/Scripts/ESRGAN/upscaleFromMemory.py
+++ IEU.Core/Scripts/ESRGAN/upscaleFromMemory.py
@@ -1,4 +1,4 @@
-#modifications from https://github.com/BlueAmulet/
+#some modifications from https://github.com/BlueAmulet/ESRGAN
 import sys
 import os.path
 import glob
@@ -28,8 +28,39 @@ model_paths = json.loads(unquote(line))
 for model_path in model_paths:
     state_dict = torch.load(model_path)
 
-    if 'conv_first.weight' in state_dict:
-        raise ValueError("Attempted to load a new architecture model")
+if 'conv_first.weight' in state_dict:
+#from https://github.com/JoeyBallentine/ESRGAN
+    print('Attempting to convert and load a new-format model')
+    sys.stdout.flush()
+    old_net = {}
+    items = []
+    for k, v in state_dict.items():
+        items.append(k)
+
+    old_net['model.0.weight'] = state_dict['conv_first.weight']
+    old_net['model.0.bias'] = state_dict['conv_first.bias']
+
+    for k in items.copy():
+        if 'RDB' in k:
+            ori_k = k.replace('RRDB_trunk.', 'model.1.sub.')
+            if '.weight' in k:
+                ori_k = ori_k.replace('.weight', '.0.weight')
+            elif '.bias' in k:
+                ori_k = ori_k.replace('.bias', '.0.bias')
+            old_net[ori_k] = state_dict[k]
+            items.remove(k)
+
+    old_net['model.1.sub.23.weight'] = state_dict['trunk_conv.weight']
+    old_net['model.1.sub.23.bias'] = state_dict['trunk_conv.bias']
+    old_net['model.3.weight'] = state_dict['upconv1.weight']
+    old_net['model.3.bias'] = state_dict['upconv1.bias']
+    old_net['model.6.weight'] = state_dict['upconv2.weight']
+    old_net['model.6.bias'] = state_dict['upconv2.bias']
+    old_net['model.8.weight'] = state_dict['HRconv.weight']
+    old_net['model.8.bias'] = state_dict['HRconv.bias']
+    old_net['model.10.weight'] = state_dict['conv_last.weight']
+    old_net['model.10.bias'] = state_dict['conv_last.bias']
+    state_dict = old_net
 
     # extract model information
     scale2 = 0
@@ -73,18 +104,18 @@ for model_path in model_paths:
         #print('NEW LINE IS {:s}'.format(line))
         #sys.stdout.flush()
         if line == 'end':
-           break    
+           break
         try:
             lrImages = json.loads(unquote(line))
         except:
-            print('INVALID LINE: {:s}'.format(line))
+            #print('INVALID LINE: {:s}'.format(line))
             #sys.stdout.flush()
             break
-        for path in lrImages:        
-            idx += 1        
+        for path in lrImages:
+            idx += 1
             base = os.path.splitext(os.path.basename(path))[0]
             inputpath = path
-            outputpath = path.replace(test_img_folder,'')        
+            outputpath = path.replace(test_img_folder,'')
             # read image
             img_original = base64.b64decode(lrImages[path])
             img_np = np.frombuffer(img_original, dtype=np.uint8);
@@ -119,24 +150,17 @@ for model_path in model_paths:
             output = (output * 255.0).round()
             
             buffer = cv2.imencode(".png", output)[1]
-            data = base64.b64encode(buffer)  
+            data = base64.b64encode(buffer)
             outpath = ''
-            #jpg_original = base64.b64decode(jpg_as_text)
-            #pg_as_np = np.frombuffer(jpg_original, dtype=np.uint8);
-            #image_buffer = cv2.imdecode(jpg_as_np, flags=1)
-            #sys.stdout.flush()        
             modelname = os.path.splitext(os.path.basename(model_path))[0]
             if outMode == '1' or outMode == '2':
                 baseinput = os.path.splitext(os.path.basename(path))[0]
-                baseinput = re.search('(.*)(_tile-[0-9]+)', baseinput, re.IGNORECASE).group(1)                
+                baseinput = re.search('(.*)(_tile-[0-9]+)', baseinput, re.IGNORECASE).group(1)
             if outMode == '1':
-                outpath = '{3:s}\Images\{0:s}\[{2:s}]_{1:s}.png'.format(baseinput, base, modelname, output_folder)                    
+                outpath = '{3:s}\Images\{0:s}\[{2:s}]_{1:s}.png'.format(baseinput, base, modelname, output_folder)
             if outMode == '2':
-                outpath = '{2:s}\Models\{0:s}\{1:s}.png'.format(modelname, base, output_folder)           
-            if outMode == '0' or outMode == '3':            
+                outpath = '{2:s}\Models\{0:s}\{1:s}.png'.format(modelname, base, output_folder)
+            if outMode == '0' or outMode == '3':
                 outpath = '{1}\{0}'.format(outputpath, output_folder)
             print('{0}:::{1}:::{2}:::{3}'.format(data, outpath, path, modelname))
             sys.stdout.flush()
-            #if mode==0:
-            #    break
-        

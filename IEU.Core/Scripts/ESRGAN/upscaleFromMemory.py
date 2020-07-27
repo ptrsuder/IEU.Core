969edae5fd8732abe6a83ff255dcdc2a2db88d68
ptrsuder <ptrsuder@gmail.com>
2020-07-27 03:59:17 +0300

    Fix tabulation

--- IEU.Core/Scripts/ESRGAN/upscaleFromMemory.py
+++ IEU.Core/Scripts/ESRGAN/upscaleFromMemory.py
@@ -28,39 +28,39 @@ model_paths = json.loads(unquote(line))
 for model_path in model_paths:
     state_dict = torch.load(model_path)
 
-if 'conv_first.weight' in state_dict:
-#from https://github.com/JoeyBallentine/ESRGAN
-    print('Attempting to convert and load a new-format model')
-    sys.stdout.flush()
-    old_net = {}
-    items = []
-    for k, v in state_dict.items():
-        items.append(k)
+    if 'conv_first.weight' in state_dict:
+    #from https://github.com/JoeyBallentine/ESRGAN
+        print('Attempting to convert and load a new-format model')
+        sys.stdout.flush()
+        old_net = {}
+        items = []
+        for k, v in state_dict.items():
+            items.append(k)
 
-    old_net['model.0.weight'] = state_dict['conv_first.weight']
-    old_net['model.0.bias'] = state_dict['conv_first.bias']
+        old_net['model.0.weight'] = state_dict['conv_first.weight']
+        old_net['model.0.bias'] = state_dict['conv_first.bias']
 
-    for k in items.copy():
-        if 'RDB' in k:
-            ori_k = k.replace('RRDB_trunk.', 'model.1.sub.')
-            if '.weight' in k:
-                ori_k = ori_k.replace('.weight', '.0.weight')
-            elif '.bias' in k:
-                ori_k = ori_k.replace('.bias', '.0.bias')
-            old_net[ori_k] = state_dict[k]
-            items.remove(k)
+        for k in items.copy():
+            if 'RDB' in k:
+                ori_k = k.replace('RRDB_trunk.', 'model.1.sub.')
+                if '.weight' in k:
+                    ori_k = ori_k.replace('.weight', '.0.weight')
+                elif '.bias' in k:
+                    ori_k = ori_k.replace('.bias', '.0.bias')
+                old_net[ori_k] = state_dict[k]
+                items.remove(k)
 
-    old_net['model.1.sub.23.weight'] = state_dict['trunk_conv.weight']
-    old_net['model.1.sub.23.bias'] = state_dict['trunk_conv.bias']
-    old_net['model.3.weight'] = state_dict['upconv1.weight']
-    old_net['model.3.bias'] = state_dict['upconv1.bias']
-    old_net['model.6.weight'] = state_dict['upconv2.weight']
-    old_net['model.6.bias'] = state_dict['upconv2.bias']
-    old_net['model.8.weight'] = state_dict['HRconv.weight']
-    old_net['model.8.bias'] = state_dict['HRconv.bias']
-    old_net['model.10.weight'] = state_dict['conv_last.weight']
-    old_net['model.10.bias'] = state_dict['conv_last.bias']
-    state_dict = old_net
+        old_net['model.1.sub.23.weight'] = state_dict['trunk_conv.weight']
+        old_net['model.1.sub.23.bias'] = state_dict['trunk_conv.bias']
+        old_net['model.3.weight'] = state_dict['upconv1.weight']
+        old_net['model.3.bias'] = state_dict['upconv1.bias']
+        old_net['model.6.weight'] = state_dict['upconv2.weight']
+        old_net['model.6.bias'] = state_dict['upconv2.bias']
+        old_net['model.8.weight'] = state_dict['HRconv.weight']
+        old_net['model.8.bias'] = state_dict['HRconv.bias']
+        old_net['model.10.weight'] = state_dict['conv_last.weight']
+        old_net['model.10.bias'] = state_dict['conv_last.bias']
+        state_dict = old_net
 
     # extract model information
     scale2 = 0
